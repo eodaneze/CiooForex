a43b1c0f88f3df8e4945f4b22afc0a72e76c5646
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-06-09 14:46:28 -0700

    designing the hero and the about section

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+// -----------back to top 
+let backtotop = document.querySelector('.back-to-top')
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > 100) {
+      backtotop.classList.add('active')
+    } else {
+      backtotop.classList.remove('active')
+    }
+})
+
+
+//--------------- end of back to top
+
+
+let nav_content = document.querySelector('.nav-content');
+window.addEventListener('scroll', () => {
+
+    if(window.pageYOffset > 100){
+        nav_content.classList.add('main-nav-change')
+    }else if(window.pageYOffset < 100){
+        nav_content.classList.remove('main-nav-change')
+    }
+})
+
+  
