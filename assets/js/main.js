4d8906219ebcc5fb6b99cd1ab967dd5aca8f23e1
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-06-12 14:10:15 -0700

    making corrections to the hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,4 +25,30 @@ window.addEventListener('scroll', () => {
     }
 })
 
+
+
+// hero-section-slider
+
+var int;
+function setInt() {
+  clearInterval(int);
+  int = setInterval(function() {
+    var btns = document.getElementsByName("carousel");
+    for(var i = 0; i < btns.length; i++) {
+      if(btns[i].checked) {
+        btns[i].checked = false;
+        if(i + 1 == btns.length) {
+          btns[0].checked = true;
+        }
+        else {
+          btns[i + 1].checked = true;
+        }
+        return;
+      }
+    }
+  }, 5000); 
+}
+setInt();
+
+
   
