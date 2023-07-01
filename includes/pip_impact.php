3bbc98a620cbf95f92d3c9bc372bf99cb77915ac
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-07-01 12:41:28 -0700

    moving the registration to a seperate page and codeing the registration logic

--- includes/pip_impact.php
+++ includes/pip_impact.php
@@ -1,8 +1,78 @@
 <?php
+require_once('./connection.php');
   if(isset($_POST['register'])){
   	 $name = $_POST['name'];
   	 $email = $_POST['email'];
+  	 $wphone = $_POST['wphone'];
+  	 $cphone = $_POST['cphone'];
+  	 $teleId = $_POST['teleId'];
+  	 $instaId = $_POST['instaId'];
+  	 $location = $_POST['location'];
+  	 $dob = $_POST['dob'];
+  	 $occupation = $_POST['occupation'];
+  	 $pc = $_POST['pc'];
+  	 $lf = $_POST['lf'];
+
+     // get users age
+      $getDob = $dob;
+      $years = (date('Y') - date('Y',strtotime($getDob)));
+      // check duplicate users by email, instagram id and telegram id;
+      $sql = "SELECT * FROM pip_impact WHERE email = '$email'";
+      $result = mysqli_query($conn, $sql);
+
+     if($name == "" || $email == "" || $wphone == "" || $cphone == "" || $teleId == "" || $instaId == "" || $location == "" || $dob == "" || $occupation == "" || $pc == "" || $lf== ""){
+        $error = urlencode("All fields are required");
+        header('location: ../pip_impact_register.php?error='.$error);
+        return false;
+     }elseif($years < 18){
+        $error = urlencode("You must be 18years and above");
+        header('location: ../pip_impact_register.php?error='.$error);
+        return false;
+     }else{
+         if($_FILES['file']['name'] != ''){
+            $filename = $_FILES['file']['name'];
+            $filetmp = $_FILES['file']['tmp_name'];
+            $filesize = $_FILES['file']['size'];
+            $filetype = $_FILES['file']['type'];
+            $fileext = explode('.', $filename);
+            $fileactualext = strtolower(end($fileext));
+
+             $allow = array('jpg', 'jpeg', 'png', 'gif');
+             if(in_array($fileactualext, $allow)){
+                 $pic = uniqid('',true).'.'.$fileactualext;
+                $filedestination = 'pip_impactImg/'.$pic;
+
+                if(move_uploaded_file($filetmp, $filedestination)){
+                     $md = date('Y-m-d');
+                     $sql = "INSERT INTO pip_impact(name, email, wphone, tphone, telId, instaId, location, dob, occupation, pic, pcon, lforex, createddate)VALUES('$name', '$email', '$wphone', '$cphone', '$teleId', '$instaId', '$location', '$dob', '$occupation', '$pic', '$pc', '$lf', '$md')";
+
+                     $result = mysqli_query($conn, $sql);
+                    if($result){
+                        $success = urlencode("Registration successful!!");
+                        header("location:../pip_impact_register.php?success=".$success);
+                        return false;
+                    }else{
+                         $error = urlencode("Error Registering For Pip Impact");
+                  header('location: ../pip_impact_register.php?error='.$error);
+                  return false;
+                    }
+                }else{
+                   $error = urlencode("Error Uploading picture");
+                  header('location: ../pip_impact_register.php?error='.$error);
+                  return false;
+                }
+
+             }else{
+                $error = urlencode("unsupported file format");
+                header('location: ../pip_impact_register.php?error='.$error);
+                return false;
+             }
+         }else{
+            $error = urlencode("please Upload a picture");
+            header('location: ../pip_impact_register.php?error='.$error);
+            return false;
+         }
+     }
   }
 
-?>
 ?>
---
+++ pip_impact_register.php
@@ -0,0 +1,99 @@
+<?php
+  $title = "Register Pip Impact";
+  require_once('./home_header.php')
+?>
+<link rel="stylesheet" type="text/css" href="./assets/css/pip_impact.css">
+
+<body style="background-color: orangered; display: flex; align-items: center; justify-content: center; width: 100%;">
+  
+    <?php
+        if(isset($_GET['error'])){
+          ?>
+    <script>
+    swal({
+        title: "<?=urldecode($_GET['error'])?>",
+        text: "Check the field",
+        icon: "error",
+        button: "Ok",
+    });
+    </script>
+    <?php
+        }elseif(isset($_GET['success'])){
+            ?>
+    <script>
+    swal({
+        title: "<?=urldecode($_GET['success'])?>",
+        text: "Welcome",
+        icon: "success",
+        button: "Ok",
+    });
+    </script>
+    <?php
+        }
+    ?>
+
+  <div class="all-register-pip bg-white w-50 p-5 my-5">
+     <form action="./includes/pip_impact.php" method="post" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="col-lg-6 mb-4">
+                                <label>Name</label>
+                                <input type="text" name="name" class="form-control">
+                            </div>
+                            <div class="col-lg-6 mb-4">
+                                <label>Email</label>
+                                <input type="email" name="email" class="form-control">
+                            </div>
+                            <div class="col-lg-6 mb-4">
+                                <label>Phone Number (Whatsapp)</label>
+                                <input type="text" name="wphone" class="form-control">
+                            </div>
+                            <div class="col-lg-6 mb-4">
+                                <label>Phone Number (Call line)</label>
+                                <input type="text" name="cphone" class="form-control">
+                            </div>
+                            <div class="col-lg-6 mb-4">
+                                <label>Telegram Id</label>
+                                <input type="text" name="teleId" class="form-control">
+                            </div>
+                            <div class="col-lg-6 mb-4">
+                                <label>Instagram Id</label>
+                                <input type="text" name="instaId" class="form-control">
+                            </div>
+                            <div class="col-lg-6 mb-4">
+                                <label>Location</label>
+                                <input type="text" name="location" class="form-control">
+                            </div>
+                            <div class="col-lg-6 mb-4">
+                                <label>Date Of Birth</label>
+                                <input type="date" name="dob" class="form-control">
+                            </div>
+                            <div class="col-lg-12 mb-4">
+                                <label>Occupation</label>
+                                <select name="occupation" class="form-control">
+                                    <option>--Select Occupation--</option>
+                                    <option>Student</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-12 mb-4">
+                                <label>Upload Picture</label>
+                                <input type="file" name="file" class="form-control">
+                                <span><b>Note: </b>Please Upload Only Portrait Picture</span>
+                            </div>
+                            <div class="col-lg-12 mb-4">
+                                <label>Have You Participated in a contest before?</label><br>
+                                <input type="radio" name="pc" value="Yes">Yes
+                                <input type="radio" name="pc" value="No">No
+                            </div>
+                            <div class="col-lg-12 mb-4">
+                                <label>Have You learnt forex before?</label><br>
+                                <input type="radio" name="lf" value="Yes">Yes
+                                <input type="radio" name="lf" value="No">No
+                            </div>
+
+                        </div>
+                        <div class="register-btn p-2 border-top">
+                            <button class="" name="register">Register</button>
+                        </div>
+                    </form>
+  </div>
+</body>
